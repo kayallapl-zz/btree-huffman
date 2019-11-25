4acee27db4a9ab4c963828916069fb2f48f31100
Kayalla <kayallapl@id.uff.br>
2019-11-24 23:51:16 -0300

    huffman tree

--- huffmantree.c
+++ huffmantree.c
@@ -1,100 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct arvhuff{
-    int frequencia, bin, vogal, maiuscula;
+typedef struct arvhuff
+{
+    int freq, bin;
     char letra;
     struct arvhuff *esq, *dir;
-}TARVHUFF
+} TARVHUFF;
 
-int englishLetterFrequencies [27] = {81, 15, 28, 43, 128, 23, 20, 61, 71, 2, 1, 40, 24, 69, 76, 20, 1, 61, 64, 91, 28, 10, 24, 1, 20, 1, 130};
-
-/*finds and returns the small sub-tree in the forrest*/
-int findSmaller (Node *array[], int differentFrom){
-    int smaller;
-    int i = 0;
-
-    while (array[i]->value==-1)
-        i++;
-    smaller=i;
-    if (i==differentFrom){
-        i++;
-        while (array[i]->value==-1)
-            i++;
-        smaller=i;
-    }
-
-    for (i=1;i<27;i++){
-        if (array[i]->value==-1)
-            continue;
-        if (i==differentFrom)
-            continue;
-        if (array[i]->value<array[smaller]->value)
-            smaller = i;
-    }
-
-    return smaller;
+TARVHUFF *cria_huff(char l, int f, TARVHUFF *e, TARVHUFF *d)
+{
+    TARVHUFF *novo = (TARVHUFF *)malloc(sizeof(TARVHUFF));
+    novo->freq = f;
+    novo->letra = l;
+    novo->esq = e;
+    novo->dir = d;
+    return novo;
 }
 
-/*builds the huffman tree and returns its address by reference*/
-void buildHuffmanTree(Node **tree){
-    Node *temp;
-    Node *array[27];
-    int i, subTrees = 27;
-    int smallOne,smallTwo;
-
-    for (i=0;i<27;i++){
-        array[i] = malloc(sizeof(Node));
-        array[i]->value = englishLetterFrequencies[i];
-        array[i]->letter = i;
-        array[i]->left = NULL;
-        array[i]->right = NULL;
-    }
-
-    while (subTrees>1){
-        smallOne=findSmaller(array,-1);
-        smallTwo=findSmaller(array,smallOne);
-        temp = array[smallOne];
-        array[smallOne] = malloc(sizeof(Node));
-        array[smallOne]->value=temp->value+array[smallTwo]->value;
-        array[smallOne]->letter=127;
-        array[smallOne]->left=array[smallTwo];
-        array[smallOne]->right=temp;
-        array[smallTwo]->value=-1;
-        subTrees--;
-    }
-
-    *tree = array[smallOne];
-
-return;
+TARVHUFF *insere_huff(char e, int f, TARVHUFF *t)
+{
+    if (!t)
+        t = cria_huff(e, f, NULL, NULL);
+    else if (f <= t->freq)
+        t->esq = insere_huff(e, f, t->esq);
+    else if (f > t->freq)
+        t->dir = insere_huff(e, f, t->dir);
+    return t;
 }
 
-int main(){
-    Node *tree;
-    int codeTable[27], codeTable2[27];
-    int compress;
-    char filename[20];
-    FILE *input, *output;
+void libera_huff(TARVHUFF *t)
+{
+    if (t)
+    {
+        libera_huff(t->esq);
+        libera_huff(t->dir);
+        free(t);
+    }
+}
 
-    buildHuffmanTree(&tree);
+TARVHUFF *busca_huff(int e, TARVHUFF *t)
+{
+    if ((!t) || ((int)t->letra == e))
+        return t;
+    TARVHUFF *resp = busca_huff(e, t->esq);
+    if (resp)
+        return resp;
+    return busca_huff(e, t->dir);
+}
 
-    fillTable(codeTable, tree, 0);
+void imprime_aux(TARVHUFF *a, int andar)
+{
+    if (a)
+    {
+        int j;
+        imprime_aux(a->esq, andar + 1);
+        for (j = 0; j <= andar; j++)
+            printf("   ");
+        printf("%c\n", a->letra);
+        imprime_aux(a->dir, andar + 1);
+    }
+}
 
-    invertCodes(codeTable,codeTable2);
+void imprime_huff(TARVHUFF *a)
+{
+    imprime_aux(a, 1);
+}
 
-    /*get input details from user*/
-    printf("Type the name of the file to process:n");
-    scanf("%s",filename);
-    printf("Type 1 to compress and 2 to decompress:n");
-    scanf("%d",&compress);
+TARVHUFF *inicializa()
+{
+    return NULL;
+}
 
-    input = fopen(filename, "r");
-    output = fopen("output.txt","w");
+int main()
+{
+    TARVHUFF *teste = inicializa();
+    char letras[9] = {'b', 'h', 'd', 'c', 'e', 'f', 'g', 'a', 'i'};
+    int frequencias[9] = {23, 11, 14, 25, 24, 17, 36, 42, 55};
+    int i;
+    for (i = 0; i < 9; i++)
+    {
+        printf("%d\n", i);
+        teste = insere_huff(letras[i], frequencias[i], teste);
+    }
 
-    if (compress==1)
-        compressFile(input,output,codeTable2);
-    else
-        decompressFile(input,output, tree);
-
-    return 0;
+    imprime_huff(teste);
 }
