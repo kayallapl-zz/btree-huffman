4d36edda4dbd38de227328510f8e3c63a6844ec4
Kayalla <kayallapl@id.uff.br>
2019-11-24 23:48:59 -0300

    modifying BTree structure

--- btree.c
+++ btree.c
@@ -3,8 +3,8 @@
 #include <string.h>
 
 typedef struct btree{
-  int nletras, folha, *letra, vogal, maiuscula;
-  float frequencia;
+  int nletras, folha, *letra;
+  float *frequencia;
   struct btree **filho;
 }BTree;
 
@@ -23,8 +23,9 @@ int eh_maiuscula(int letra){
   return -1; //caso não seja alfabético
 }
 
-void qs_simultaneo(char *letras, int *freq, int esq, int dir){
+void qs_simultaneo(char *letras, float *freq, int esq, int dir){
   int i, j, x, y;
+  float f;
   i = esq;
   j = dir;
   x = letras[(esq + dir) / 2];
@@ -35,9 +36,9 @@ void qs_simultaneo(char *letras, int *freq, int esq, int dir){
       y = letras[i]; //realiza troca no array de letras
       letras[i] = letras[j];
       letras[j] = y;
-      y = freq[i]; //realiza troca no array de frequencias
+      f = freq[i]; //realiza troca no array de frequencias
       freq[i] = freq[j];
-      freq[j] = y;
+      freq[j] = f;
       i++;
       j--;
     }
@@ -54,9 +55,8 @@ BTree *cria(int t){
   BTree* novo = (BTree*)malloc(sizeof(BTree));
   novo->nletras = 0;
   novo->letra =(int*)malloc(sizeof(int*)*((t*2)-1));
+  novo->frequencia =(float*)malloc(sizeof(float*)*((t*2)-1));
   novo->folha=1;
-  novo->vogal=-1;
-  novo->maiuscula=-1;
   novo->filho = (BTree**)malloc(sizeof(BTree*)*t*2);
   int i;
   for(i=0; i<(t*2); i++) novo->filho[i] = NULL;
@@ -73,6 +73,16 @@ BTree *busca_letra(BTree* arvore, int letra){
   return busca_letra(arvore->filho[i], letra);
 }
 
+int busca_pos(BTree* arvore, int letra){
+  int resp = -1;
+  if(!arvore) return resp;
+  int i = 0;
+  while(i < arvore->nletras && letra > arvore->letra[i]) i++;
+  if(i < arvore->nletras && letra == arvore->letra[i]) return i;
+  if(arvore->folha) return resp;
+  return busca_pos(arvore->filho[i], letra);
+}
+
 void busca_subordinadas(BTree* arvore, int letra){
   if(!arvore){
     printf("Árvore não existe.\n");
@@ -82,12 +92,12 @@ void busca_subordinadas(BTree* arvore, int letra){
   while(i < arvore->nletras && letra > arvore->letra[i]) i++;
   if(i < arvore->nletras && letra == arvore->letra[i]){
     char *vogal = "É vogal.";
-    if (!arvore->vogal) vogal = "É consoante.";
-    else if (arvore->vogal == -1) vogal = "Não é alfabético.";
+    if (!eh_vogal(letra)) vogal = "É consoante.";
+    else if (eh_vogal(letra) == -1) vogal = "Não é alfabético.";
     char *maiuscula = "É maiúscula.";
-    if (!arvore->maiuscula) maiuscula = "É minúscula.";
-    else if (arvore->maiuscula == -1) maiuscula = "Não é alfabético.";
-    printf("letra: %c \nfrequencia: %f\n%s\n%s\n", arvore->letra[0], arvore->frequencia, vogal, maiuscula);
+    if (!eh_maiuscula(letra)) maiuscula = "É minúscula.";
+    else if (eh_maiuscula(letra) == -1) maiuscula = "Não é alfabético.";
+    printf("letra: %c \nfrequencia: %f\n%s\n%s\n", arvore->letra[i], arvore->frequencia[i], vogal, maiuscula);
     return;
   }
   if (!arvore->folha){
@@ -114,7 +124,10 @@ BTree *divisao(BTree *arvore_a, int pos, BTree* arvore_b, int t){
   nova->nletras = t-1;
   nova->folha = arvore_b->folha;
   int i;
-  for(i = 0; i < t-1; i++) nova->letra[i] = arvore_b->letra[i+t];
+  for(i = 0; i < t-1; i++) {
+    nova->letra[i] = arvore_b->letra[i+t];
+    nova->frequencia[i] = arvore_b->frequencia[i+t];
+  }
   if(!arvore_b->folha){
     for(i = 0; i < t; i++){
       nova->filho[i] = arvore_b->filho[i+t];
@@ -124,20 +137,26 @@ BTree *divisao(BTree *arvore_a, int pos, BTree* arvore_b, int t){
   arvore_b->nletras = t-1;
   for(i = arvore_a->nletras; i >= pos; i--) arvore_a->filho[i+1] = arvore_a->filho[i];
   arvore_a->filho[pos] = nova;
-  for(i = arvore_a->nletras; i >= pos; i--) arvore_a->letra[i] = arvore_a->letra[i-1];
+  for(i = arvore_a->nletras; i >= pos; i--) {
+    arvore_a->letra[i] = arvore_a->letra[i-1];
+    arvore_a->frequencia[i] = arvore_a->frequencia[i-1];
+  }
   arvore_a->letra[pos-1] = arvore_b->letra[t-1];
+  arvore_a->frequencia[pos-1] = arvore_b->frequencia[t-1];
   arvore_a->nletras++;
   return arvore_a;
 }
 
-BTree *insere_nao_completo(BTree *arvore, int letra, int t){
+BTree *insere_nao_completo(BTree *arvore, int letra, float freq, int t){
   int i = arvore->nletras-1;
   if(arvore->folha){ //se é folha, vai procurando até achar o primeiro menor que ele mesmo, de trás pra frente. então insere
     while((i >= 0) && (letra < arvore->letra[i])){
       arvore->letra[i+1] = arvore->letra[i]; //vai passando a folha pro lado
+      arvore->frequencia[i+1] = arvore->frequencia[i]; //vai passando a folha pro lado
       i--;
     }
     arvore->letra[i+1] = letra; //insiro na posição seguinte do primeiro menor
+    arvore->frequencia[i+1] = freq; 
     arvore->nletras++;
     return arvore;
   }
@@ -147,7 +166,7 @@ BTree *insere_nao_completo(BTree *arvore, int letra, int t){
     arvore = divisao(arvore, (i+1), arvore->filho[i], t);
     if(letra > arvore->letra[i]) i++; //pega a posição pra inserir o filho
   }
-  arvore->filho[i] = insere_nao_completo(arvore->filho[i], letra, t);
+  arvore->filho[i] = insere_nao_completo(arvore->filho[i], letra, freq, t);
   return arvore;
 }
 
@@ -156,10 +175,8 @@ BTree *insere(BTree *arvore, int letra, float freq, int t){
   if(!arvore){ //caso não exista ainda, cria
     arvore=cria(t);
     arvore->letra[0] = letra;
+    arvore->frequencia[0] = freq;
     arvore->nletras=1;
-    arvore->frequencia=freq;
-    arvore->vogal=eh_vogal(letra);
-    arvore->maiuscula=eh_maiuscula(letra);
     return arvore;
   }
   if(arvore->nletras == (2*t)-1){ //caso esteja cheia, faz a divisão antes de inserir
@@ -168,10 +185,10 @@ BTree *insere(BTree *arvore, int letra, float freq, int t){
     nova->folha = 0;
     nova->filho[0] = arvore;
     nova = divisao(nova,1,arvore,t);
-    nova = insere_nao_completo(nova,letra,t);
+    nova = insere_nao_completo(nova,letra,freq,t);
     return nova;
   }
-  arvore = insere_nao_completo(arvore,letra,t); 
+  arvore = insere_nao_completo(arvore,letra,freq,t); 
   return arvore;
 }
 
@@ -227,7 +244,9 @@ char *descodifica_palavra(BTree *arvore, char *codigo){
   char *palavra = malloc(sizeof(char) * tam_palavra);
   strcpy(palavra, "");
   while (i < tam_palavra){
-    if (codigo[i] != '?'){
+    if (codigo[i] == '?'){
+      strcat(palavra, "?");
+    } else {
       int altura = codigo[i] - '0'; // para parsear o char em int - pq se não ele pega o int correspondente da ascii
       BTree *aux = arvore;
       int j = 0;
@@ -243,8 +262,6 @@ char *descodifica_palavra(BTree *arvore, char *codigo){
       letra[0] = aux->letra[pos];
       strcat(palavra, letra);
     }
-    else strcat(palavra, "?");
-    
     i++;
   }
 
@@ -253,28 +270,221 @@ char *descodifica_palavra(BTree *arvore, char *codigo){
   return palavra;
 }
 
+BTree* remover(BTree* arvore, int letra, int t){
+  if(!arvore) return arvore;
+  int i;
+  printf("Removendo %d...\n", letra);
+  for(i = 0; i<arvore->nletras && arvore->letra[i] < letra; i++);
+  if(i < arvore->nletras && letra == arvore->letra[i]){ //CASOS 1, 2A, 2B e 2C
+    if(arvore->folha){ //CASO 1
+      printf("\nCASO 1\n");
+      int j;
+      for(j=i; j<arvore->nletras-1;j++) arvore->letra[j] = arvore->letra[j+1];
+      arvore->nletras--;
+      return arvore;      
+    }
+    if(!arvore->folha && arvore->filho[i]->nletras >= t){ //CASO 2A
+      printf("\nCASO 2A\n");
+      BTree *y = arvore->filho[i];  //Encontrar o predecessor k' de k na árvore com raiz em y
+      while(!y->folha) y = y->filho[y->nletras];
+      int temp = y->letra[y->nletras-1];
+      arvore->filho[i] = remover(arvore->filho[i], temp, t); 
+      //Eliminar recursivamente K e substitua K por K' em x
+      arvore->letra[i] = temp;
+      return arvore;
+    }
+    if(!arvore->folha && arvore->filho[i+1]->nletras >= t){ //CASO 2B
+      printf("\nCASO 2B\n");
+      BTree *y = arvore->filho[i+1];  //Encontrar o sucessor k' de k na árvore com raiz em y
+      while(!y->folha) y = y->filho[0];
+      int temp = y->letra[0];
+      y = remover(arvore->filho[i+1], temp, t); //Eliminar recursivamente K e substitua K por K' em x
+      arvore->letra[i] = temp;
+      return arvore;
+    }
+    if(!arvore->folha && arvore->filho[i+1]->nletras == t-1 && arvore->filho[i]->nletras == t-1){ //CASO 2C
+      printf("\nCASO 2C\n");
+      BTree *y = arvore->filho[i];
+      BTree *z = arvore->filho[i+1];
+      y->letra[y->nletras] = letra;          //colocar letra ao final de filho[i]
+      int j;
+      for(j=0; j<t-1; j++)                //juntar letra[i+1] com letra[i]
+        y->letra[t+j] = z->letra[j];
+      for(j=0; j<=t; j++)                 //juntar filho[i+1] com filho[i]
+        y->filho[t+j] = z->filho[j];
+      y->nletras = 2*t-1;
+      for(j=i; j < arvore->nletras-1; j++)   //remover letra de arvore
+        arvore->letra[j] = arvore->letra[j+1];
+      for(j=i+1; j <= arvore->nletras; j++)  //remover ponteiro para filho[i+1]
+        arvore->filho[j] = arvore->filho[j+1];
+      arvore->filho[j] = NULL; //Campello
+      arvore->nletras--;
+      arvore->filho[i] = remover(arvore->filho[i], letra, t);
+      return arvore;   
+    }   
+  }
+
+  BTree *y = arvore->filho[i], *z = NULL;
+  if (y->nletras == t-1){ //CASOS 3A e 3B
+    if((i < arvore->nletras) && (arvore->filho[i+1]->nletras >=t)){ //CASO 3A
+      printf("\nCASO 3A: i menor que nletras\n");
+      z = arvore->filho[i+1];
+      y->letra[t-1] = arvore->letra[i];   //dar a y a letra i da arvore
+      y->nletras++;
+      arvore->letra[i] = z->letra[0];     //dar a arvore uma letra de z
+      int j;
+      for(j=0; j < z->nletras-1; j++)  //ajustar chaves de z
+        z->letra[j] = z->letra[j+1];
+      //z->letra[j] = 0; Rosseti
+      y->filho[y->nletras] = z->filho[0]; //enviar ponteiro menor de z para o novo elemento em y
+      for(j=0; j < z->nletras; j++)       //ajustar filhos de z
+        z->filho[j] = z->filho[j+1];
+      z->nletras--;
+      arvore->filho[i] = remover(arvore->filho[i], letra, t);
+      return arvore;
+    }
+    if((i > 0) && (!z) && (arvore->filho[i-1]->nletras >=t)){ //CASO 3A
+      printf("\nCASO 3A: i igual a nletras\n");
+      z = arvore->filho[i-1];
+      int j;
+      for(j = y->nletras; j>0; j--)               //encaixar lugar da nova letra
+        y->letra[j] = y->letra[j-1];
+      for(j = y->nletras+1; j>0; j--)             //encaixar lugar dos filhos da nova letra
+        y->filho[j] = y->filho[j-1];
+      y->letra[0] = arvore->letra[i-1];              //dar a y a letra i da arvore
+      y->nletras++;
+      arvore->letra[i-1] = z->letra[z->nletras-1];   //dar a arvore uma letra de z
+      y->filho[0] = z->filho[z->nletras];         //enviar ponteiro de z para o novo elemento em y
+      z->nletras--;
+      arvore->filho[i] = remover(y, letra, t);
+      return arvore;
+    }
+    if(!z){ //CASO 3B
+      if(i < arvore->nletras && arvore->filho[i+1]->nletras == t-1){
+        printf("\nCASO 3B: i menor que nletras\n");
+        z = arvore->filho[i+1];
+        y->letra[t-1] = arvore->letra[i];     //pegar letra [i] e coloca ao final de filho[i]
+        y->nletras++;
+        int j;
+        for(j=0; j < t-1; j++){
+          y->letra[t+j] = z->letra[j];     //passar filho[i+1] para filho[i]
+          y->nletras++;
+        }
+        if(!y->folha){
+          for(j=0; j<t; j++){
+            y->filho[t+j] = z->filho[j];
+          }
+        }
+        for(j=i; j < arvore->nletras-1; j++){ //limpar referências de i
+          arvore->letra[j] = arvore->letra[j+1];
+          arvore->filho[j+1] = arvore->filho[j+2];
+        }
+        arvore->nletras--;
+        arvore = remover(arvore, letra, t);
+        return arvore;
+      }
+      if((i > 0) && (arvore->filho[i-1]->nletras == t-1)){ 
+        printf("\nCASO 3B: i igual a nletras\n");
+        z = arvore->filho[i-1];
+        if(i == arvore->nletras)
+          z->letra[t-1] = arvore->letra[i-1]; //pegar letra[i] e poe ao final de filho[i-1]
+        else
+          z->letra[t-1] = arvore->letra[i];   //pegar letra [i] e poe ao final de filho[i-1]
+        z->nletras++;
+        int j;
+        for(j=0; j < t-1; j++){
+          z->letra[t+j] = y->letra[j];     //passar filho[i+1] para filho[i]
+          z->nletras++;
+        }
+        if(!z->folha){
+          for(j=0; j<t; j++){
+            z->filho[t+j] = y->filho[j];
+          }
+        }
+        arvore->nletras--;
+        arvore->filho[i-1] = z;
+        arvore = remover(arvore, letra, t);
+        return arvore;
+      }
+    }
+  }  
+  arvore->filho[i] = remover(arvore->filho[i], letra, t);
+  return arvore;
+}
+
+BTree* retira(BTree* arvore, int k, int t){
+  if(!arvore || !busca_letra(arvore, k)) return arvore;
+  return remover(arvore, k, t);
+}
+
+void troca_frequencia(BTree* arvore, int letra, float freq){
+  if(!arvore){
+    printf("Árvore não existe.\n");
+    return;
+  }
+  BTree *a = busca_letra(arvore, letra);
+  if (!a){
+    printf("\nLetra não existe");
+  } else {
+    int pos = busca_pos(arvore, letra);
+    a->frequencia[pos] = freq;
+    busca_subordinadas(arvore, letra);
+  }
+}
+
+
+void busca_categoria(BTree* arvore, int categoria){
+  if(arvore){
+    int i,j,*vetor, contador=0;
+    for(i = 0; i <= arvore->nletras-1; i++){
+      busca_categoria(arvore->filho[i], categoria);
+      if ((categoria == 1) && (eh_maiuscula(arvore->letra[i])) && (eh_maiuscula(arvore->letra[i])) != -1){ //maiusculas
+        printf("%c ", arvore->letra[i]);
+      }
+      else if ((categoria == 2) && (!eh_maiuscula(arvore->letra[i]))){ //minusculas
+        printf("%c ", arvore->letra[i]);
+      }
+      else if ((categoria == 3) && (eh_vogal(arvore->letra[i])) && (eh_vogal(arvore->letra[i])) != -1)
+      { //vogais
+        printf("%c ", arvore->letra[i]);
+      }
+      else if ((categoria == 4) && (!eh_vogal(arvore->letra[i]))){ //consoantes
+        printf("%c ", arvore->letra[i]);
+      }
+      else if ((categoria == 5) && (eh_vogal(arvore->letra[i]) == -1)){ //outros caracteres
+        printf("%c ", arvore->letra[i]);
+      }
+    }
+    busca_categoria(arvore->filho[i], categoria);
+  }
+}
+
 int main(){
   BTree *arvore = inicializa();
-  char letras[9] = {'b', 'h', 'd', 'c', 'e', 'f', 'g', 'a', 'i'};
-  int frequencias[9] = {1, 1, 1, 2, 3, 2, 1, 4, 3};
-  qs_simultaneo(letras, frequencias, 0, 8);
+  char letras[11] = {'b', 'h', 'd', 'c', 'e', 'f', 'g', 'a', 'i', '!', 'A'};
+  float frequencias[11] = {1.1, 1.3, 1.4, 2.5, 3.6, 2.8, 1.9, 4.0, 3.0, 4.5, 6.7};
+  qs_simultaneo(letras, frequencias, 0, 10);
   int i;
   int t = 2;
-  for (i=0; i<9; i++){
+  for (i=0; i<11; i++){
     arvore = insere(arvore, letras[i], frequencias[i], t);
   }
-  busca_subordinadas(arvore, 'a');
+  busca_subordinadas(arvore, 'b');
   // int altura = altura_letra(arvore, 0, 'a');
   // char *codigo = codifica_letra(arvore, 'a');
   // printf("%s\n", codigo);
   // printf("altura: %d\n", altura);
   // printf("tam: %ld\n", strlen(codigo));
-
-  char palavra[] = "abada h";
-  printf("palavra inicial: %s\n", palavra);
-  char *codigo = codifica_palavra(arvore, palavra);
-  printf("palavra encriptada: %s\n", codigo);
-  char *palavra_desencriptada = descodifica_palavra(arvore, codigo);
-  printf("palavra desencriptada: %s\n", palavra_desencriptada);
+  // char palavra[] = "abada h";
+  // printf("palavra inicial: %s\n", palavra);
+  // char *codigo = codifica_palavra(arvore, palavra);
+  // printf("palavra encriptada: %s\n", codigo);
+  // char *palavra_desencriptada = descodifica_palavra(arvore, codigo);
+  // printf("palavra desencriptada: %s\n", palavra_desencriptada);
+  // arvore = remover(arvore, 'a', t);
+  troca_frequencia(arvore, 'b', 3.428);
+  printf("Categoria: \n");
+  busca_categoria(arvore, 3);
+  printf("\n");
   return 1;
 }
